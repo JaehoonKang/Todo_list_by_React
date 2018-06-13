503d214e05e400955911d732e52d22ba189a13f8
강재훈 <kangjaehoon@gangjaehun-ui-MacBook-Pro.local>
2018-06-13 11:40:23 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,120 +1,111 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"; // 노드 모듈스 안에 라이브러리 모듈을 쓸 떄는 경로를 안쓴다.
+import axios from 'axios';
 
-let count = 0;
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm';
+
+
+const todoAPI = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+})
 
 class App extends Component {
   state = {
+    loading: false,
     todos: [
-      {
-        id: count++,
-        body: 'React 공부',
-        complete: true
-      },
-      {
-        id: count++,
-        body: 'Redux 공부',
+      // {
+      //   id: count++,
+      //   body: "React 공부",
+      //   complete: true
+      // },
+      // {
+      //   id: count++,
+      //   body: "Redux 공부",
+      //   complete: false
+      // }
+    ]
+  };
+  
+  async componentDidMount() {
+    await this.fetchTodos();
+  }
+
+  
+  fetchTodos = async () => {
+    this.setState({
+      loading: true
+    })
+    const res = await todoAPI.get('/todos')
+    this.setState({
+      todos: res.data,
+      loading: false
+    }) 
+  }
+
+
+  createTodo = async newTodoBody => {
+    if (newTodoBody) {
+      const newTodo = {
+        body: newTodoBody,
         complete: false
       }
-    ],
-    newTodoBody:''
-  }
-
-  handleInputChange = e => {
-    this.setState({
-      newTodoBody: e.target.value
-    })
-  }
-
-  handleButtonClick = e => {
-    if(this.state.newTodoBody){
-      const newTodo = {
-        body: this.state.newTodoBody,
-        complete: false,
-        id: count++
-      }
-
       this.setState({
-        todos: [
-          ...this.state.todos,
-          newTodo
-        ],
-        newTodoBody: ''
-      });
-    }
-  }
-
-  handleTodoItemComplete = id => {
-    this.setState({
-        todos: this.state.todos.map(t => {
-          const newTodo = {
-            ...t
-          };
-          if(t.id === id){
-            newTodo.complete = true;
-          }
-        return newTodo
+        loading: true
       })
-    })
-  }
+      await todoAPI.post('/todos', newTodo)
+      await this.fetchTodos()
+    }
+  };
 
-  handleTodoItemDelete = id => {
+ deleteTodo = async (id, body) => {
+   this.setState({
+     loading: true
+   });
+   await todoAPI.patch(`/todos/${id}`, {
+     body
+   })
+   await this.fetchTodos();
+ }
+
+ updateTodoBody = async id => {
     this.setState({
-      todos: this.state.todos.filter(t => id !== t.id)
+      loading: true
     })
+    await todoAPI.patch(`/todos/${id}`, {
+      complete: true
+    })
+    await this.fetchTodos();
+  };
 
+  completeTodo = async id => {
+    this.setState({
+      loading: true
+    })
+    await todoAPI.delete(`todos/${id}`)
+    await this.fetchTodos();
   }
-
   render() {
-    const {todos, newTodoBody} = this.state;
-
+    const { todos, loading } = this.state;
     return (
       <div>
-        <h1>할일 목록</h1>
-        <label>
-          새 할일
-          <input type="text" value={newTodoBody} onChange={this.handleInputChange}/>
-          <button onClick={this.handleButtonClick}>추가</button>
-          </label>
+        <h1>할 일 목록</h1>
+        <TodoForm onCreate={this.createTodo}/>
+        {loading ? (
+          <div>loading...</div>
+        ) : (
+          <TodoList 
+            todos={todos}
+            onTodoComplete={this.completeTodo} 
+            onTodoDelete={this.deleteTodo}
+            onTodoBodyUpdate={this.updateTodoBody}
+          />
+          )}
 
-        <ul>
-          {
-            todos.map(todo => (
-            <TodoItem
-            key={todo.id}
-            {...todo}
-            onComplete={this.handleTodoItemComplete} 
-            onDelete={this.handleTodoItemDelete} 
-            />
-            ))
-          }
-        </ul>
       </div>
     );
   }
 }
 
 
-class TodoItem extends Component{
-  render(){
-    const {id, body, complete, onComplete, onDelete} = this.props;
 
-    return( 
-    <li className={complete ? 'complete': ''} key={id}>
-      {body}
-      <button onClick={e => {
-        onComplete(id);
-      }}>완료</button>
-
-      <button onClick={e => {
-        // this.setState({
-        //   todos: todos.filter(t => id !== t.id)
-        // })
-        onDelete(id);
-      }}>삭제</button>
-    </li>
-    )
-  }
-    
-
-}
 export default App;
